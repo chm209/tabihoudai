4f4e3c0552b0f83d998061a7565b287695f2bf74
jsg1017 <dltksk1324@gmail.com>
2023-10-31 15:21:06 +0900

    ProfileEditController added

--- src/main/java/com/tabihoudai/tabihoudai_api/TabihoudaiApiApplication.java
+++ src/main/java/com/tabihoudai/tabihoudai_api/TabihoudaiApiApplication.java
@@ -2,8 +2,12 @@ package com.tabihoudai.tabihoudai_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableJpaAuditing
 public class TabihoudaiApiApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/tabihoudai/tabihoudai_api/api/users/ProfileEditController.java
@@ -0,0 +1,39 @@
+package com.tabihoudai.tabihoudai_api.api.users;
+
+import com.tabihoudai.tabihoudai_api.entity.users.UsersEntity;
+import com.tabihoudai.tabihoudai_api.repository.users.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+
+@RestController
+@Slf4j
+@RequestMapping("/api/editprofile")
+@RequiredArgsConstructor
+public class ProfileEditController {
+
+    private final UsersRepository usersRepository;
+
+    @DeleteMapping("/withdrawal/{userIdx}")
+    public void withdrawal(@PathVariable String userIdx) {
+        String uuid;
+        uuid = strToUUID(userIdx, "-", 8);
+        uuid = strToUUID(uuid, "-", 13);
+        uuid = strToUUID(uuid, "-", 18);
+        uuid = strToUUID(uuid, "-", 23);
+
+        usersRepository.deleteById(UUID.fromString(uuid));
+    }
+
+    @PutMapping("")
+    public void editProfile(@RequestBody UsersEntity usersEntity){
+
+    }
+
+    public String strToUUID(String str, String value, int idx){
+        return str.substring(0, idx) + value + str.substring(idx);
+    }
+}
