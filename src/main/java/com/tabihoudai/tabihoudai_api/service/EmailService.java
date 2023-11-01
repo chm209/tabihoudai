dac074ba4d7bce8a35fc2c1b43d1ef39be3553d6
chanyoung <cksdud0813@gmail.com>
2023-11-01 17:17:50 +0900

    보안문제 해결

---
+++ src/main/java/com/tabihoudai/tabihoudai_api/service/EmailService.java
@@ -0,0 +1,11 @@
+package com.tabihoudai.tabihoudai_api.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+}
