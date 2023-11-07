072dd3753ff01b47d0a99239dd6f40f55093fd2a
jsg1017 <dltksk1324@gmail.com>
2023-11-07 14:17:35 +0900

    PlanController 지역,도시,명소 찾기 기능 추가

--- src/main/java/com/tabihoudai/tabihoudai_api/api/plan/PlanController.java
+++ src/main/java/com/tabihoudai/tabihoudai_api/api/plan/PlanController.java
@@ -2,12 +2,17 @@ package com.tabihoudai.tabihoudai_api.api.plan;
 
 import com.tabihoudai.tabihoudai_api.application.plan.PlanService;
 import com.tabihoudai.tabihoudai_api.dao.plan.PlanRepository;
+import com.tabihoudai.tabihoudai_api.dto.attraction.AttractionListDTO;
 import com.tabihoudai.tabihoudai_api.dto.plan.PlanDTO;
+import com.tabihoudai.tabihoudai_api.entity.attraction.AttractionEntity;
 import com.tabihoudai.tabihoudai_api.entity.plan.PlanEntity;
+import com.tabihoudai.tabihoudai_api.repository.attraction.AttractionRepository;
+import com.tabihoudai.tabihoudai_api.repository.attraction.RegionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,6 +23,8 @@ public class PlanController {
 
     private final PlanService planService;
     private final PlanRepository planRepository;
+    private final RegionRepository regionRepository;
+    private final AttractionRepository attractionRepository;
 
     @DeleteMapping("/delete/{planIdx}")
     public void delete(@PathVariable("planIdx") Long planIdx){
@@ -35,4 +42,23 @@ public class PlanController {
         log.info(planDTO.toString());
         return planService.create(planDTO);
     }
+
+    @GetMapping("/getarea")
+    public List<String> getArea() {
+        return regionRepository.findArea();
+    }
+
+    @GetMapping("/getcity")
+    public List<String> getCity(@RequestParam String region) {
+        return regionRepository.findCity(region);
+    }
+
+    @GetMapping("/getattr")
+    public List<Object[]> getAttr(@RequestParam String city) {
+        log.info(regionRepository.findRegionIdx(city).toString());
+        Long regionIdx = regionRepository.findRegionIdx(city);
+
+        return attractionRepository.getAttractionListByRegionIdx(regionIdx);
+    }
 }
+
