fc0bda91bbb5f0f24a06e03a44874e78ee8ece73
appli <ungtank@gmail.com>
2023-11-08 14:38:08 +0900

    Board : image upload controller

---
+++ src/main/java/com/tabihoudai/tabihoudai_api/controller/ImageController.java
@@ -0,0 +1,53 @@
+package com.tabihoudai.tabihoudai_api.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
+
+import java.io.File;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/image")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+public class ImageController {
+
+    @ResponseBody
+    @PostMapping("/upload")
+    public Map<String, Object> uploadImage(@RequestParam Map<String, Object> paramMap, MultipartRequest request) throws Exception {
+        MultipartFile uploadFile = request.getFile("upload");
+        String uploadDir = "H:\\Git_Tabihoudai\\tabihoudai_front\\src\\assets\\images\\";
+        uploadDir.replace("\\", "/");
+        String uploadId = UUID.randomUUID().toString() + "." + getExtensionByStringHandling(uploadFile.getOriginalFilename()).get();
+        uploadFile.transferTo(new File(uploadDir + uploadId));
+        paramMap.put("url", "/src/assets/images/" + uploadId);
+        log.info(paramMap.toString());
+        return paramMap;
+    }
+    public Optional<String> getExtensionByStringHandling(String filename) {
+        return Optional.ofNullable(filename)
+                .filter(f -> f.contains("."))
+                .map(f -> f.substring(filename.lastIndexOf(".") + 1));
+    }
+
+
+}
+//    private String makeFolder() {
+//        String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+//
+//        String folderPath = str.replace("/", File.separator);
+//
+//        File uploadPathFolder = new File(uploadPath, folderPath);
+//
+//        if (uploadPathFolder.exists() == false){
+//            uploadPathFolder.mkdirs();
+//        }
+//        return folderPath;
+//    }
+
