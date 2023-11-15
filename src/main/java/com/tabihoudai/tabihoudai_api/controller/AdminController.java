6dff0d960276616e385f6e5352fec02ff0599505
chm209 <changmin7537@gmail.com>
2023-11-15 16:13:49 +0900

    Security: Controller, Authentication 추가, 개발 완료
    * 로그인은 포트폴리오에 없지만, 필터 처리 테스트를 위해 로그인 추가
    * 토큰 받아서 admin 요청할때 access 토큰 받아서 인증하도록 처리
    *****
    * 개발 완료

--- src/main/java/com/tabihoudai/tabihoudai_api/controller/AdminController.java
+++ src/main/java/com/tabihoudai/tabihoudai_api/controller/AdminController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("api/admin")
 @RequiredArgsConstructor
+@Secured("ROLE_ADMIN")
 public class AdminController {
 
     private final AdminServices adminServices;
