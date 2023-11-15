79f93d915ef2b40a7f0cc711e68d644500790e24
chm209 <changmin7537@gmail.com>
2023-11-15 15:53:18 +0900

    Security: 진행중
    * Spring Security, JWT dependencies 추가
    * Config 파일 추가

---
+++ src/main/java/com/tabihoudai/tabihoudai_api/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.tabihoudai.tabihoudai_api.config;
+
+import com.tabihoudai.tabihoudai_api.jwt.filter.JwtAuthenticationFilter;
+import com.tabihoudai.tabihoudai_api.jwt.service.JwtService;
+import com.tabihoudai.tabihoudai_api.security.handler.JwtAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(handlingConfigures -> handlingConfigures.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/api/main").permitAll()
+                                .requestMatchers("/api/admin").hasRole("ADMIN") // 처음에 'admin/**' 로 경로를 줬는데 401 에러가 발생, admin으로 하는게 맞음
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
