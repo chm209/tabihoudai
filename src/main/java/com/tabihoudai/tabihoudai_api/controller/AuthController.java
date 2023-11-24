8715a651d390326be75fb2f462efe9cc42577e01
chm209 <changmin7537@gmail.com>
2023-11-24 09:12:25 +0900

    Auth: Controller, Authority 추가
    * log 코드 삭제 및 권한 추가

--- src/main/java/com/tabihoudai/tabihoudai_api/controller/AuthController.java
+++ src/main/java/com/tabihoudai/tabihoudai_api/controller/AuthController.java
@@ -3,24 +3,18 @@ package com.tabihoudai.tabihoudai_api.controller;
 import com.tabihoudai.tabihoudai_api.dto.AuthDTO;
 import com.tabihoudai.tabihoudai_api.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/auth")
 @RequiredArgsConstructor
-@Slf4j
 public class AuthController {
 
     private final AuthenticationService authenticationService;
 
     @PostMapping("/login")
     public AuthDTO.JsonWebTokenResponse auth(@Validated @RequestBody AuthDTO.AuthenticationRequest authRequset) {
-        log.info("=================================");
         return authenticationService.auth(authRequset);
     }
 }
