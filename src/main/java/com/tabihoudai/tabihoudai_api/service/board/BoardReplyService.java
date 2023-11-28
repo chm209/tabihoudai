d53b536ad88217992a91250fabe5005d089f2820
appli <ungtank@gmail.com>
2023-11-28 10:01:19 +0900

    BoardReply : get reply service 작성

--- src/main/java/com/tabihoudai/tabihoudai_api/service/board/BoardReplyService.java
+++ src/main/java/com/tabihoudai/tabihoudai_api/service/board/BoardReplyService.java
@@ -1,4 +1,20 @@
 package com.tabihoudai.tabihoudai_api.service.board;
 
+import com.tabihoudai.tabihoudai_api.dto.board.BoardReplyDTO;
+import com.tabihoudai.tabihoudai_api.entity.board.BoardReplyEntity;
+import com.tabihoudai.tabihoudai_api.entity.users.UsersEntity;
+
+import java.util.List;
+
 public interface BoardReplyService {
+    List<BoardReplyDTO.getReplyDTO> getReply(Long boardIdx);
+
+    default BoardReplyDTO.getReplyDTO entityToGetDTO(BoardReplyEntity boardReply, UsersEntity users){
+        return BoardReplyDTO.getReplyDTO.builder()
+                .nickname(users.getNickname())
+                .content(boardReply.getContent())
+                .profile(users.getProfile())
+                .regDate(boardReply.getRegDate())
+                .build();
+    }
 }
--- src/main/java/com/tabihoudai/tabihoudai_api/service/board/BoardReplyServiceImpl.java
+++ src/main/java/com/tabihoudai/tabihoudai_api/service/board/BoardReplyServiceImpl.java
@@ -1,9 +1,15 @@
 package com.tabihoudai.tabihoudai_api.service.board;
 
+import com.tabihoudai.tabihoudai_api.dto.board.BoardReplyDTO;
+import com.tabihoudai.tabihoudai_api.entity.board.BoardReplyEntity;
+import com.tabihoudai.tabihoudai_api.entity.users.UsersEntity;
 import com.tabihoudai.tabihoudai_api.repository.board.BoardReplyRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +17,12 @@ import org.springframework.stereotype.Service;
 public class BoardReplyServiceImpl implements BoardReplyService{
 
     private final BoardReplyRepository boardReplyRepository;
+
+    @Override
+    public List<BoardReplyDTO.getReplyDTO> getReply(Long boardIdx){
+        List<Object[]> result = boardReplyRepository.getReplyByBoardIdx(boardIdx);
+        List<BoardReplyDTO.getReplyDTO> collect = result.stream().map(objects -> entityToGetDTO((BoardReplyEntity) objects[0], (UsersEntity) objects[1]))
+                .collect(Collectors.toList());
+        return collect;
+    }
 }
