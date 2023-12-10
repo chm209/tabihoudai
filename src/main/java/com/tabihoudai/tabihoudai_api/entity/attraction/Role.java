0ce4939e576fdd9227be15d6464a150dfc592233
swKim1996 <dhsesw36@gmail.com>
2023-12-10 09:40:23 +0900

    Security : UserEntity변경
    Spring Security를 위해 UserEntity변경 및 Role추가

---
+++ src/main/java/com/tabihoudai/tabihoudai_api/entity/attraction/Role.java
@@ -0,0 +1,6 @@
+package com.tabihoudai.tabihoudai_api.entity.attraction;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/tabihoudai/tabihoudai_api/entity/attraction/UserEntity.java
+++ src/main/java/com/tabihoudai/tabihoudai_api/entity/attraction/UserEntity.java
@@ -2,6 +2,12 @@ package com.tabihoudai.tabihoudai_api.entity.attraction;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Builder
@@ -9,7 +15,7 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "tbl_user")
 @Getter
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE,
@@ -34,5 +40,41 @@ public class UserEntity {
     @Column(nullable = false)
     private String nickname;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return pw;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
